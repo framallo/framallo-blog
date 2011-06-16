cfa5228847b678a59c63af18bfbcd58c54f39363
Federico Ramallo <framallo@gmail.com>
2011-06-16 16:29:33 -0500

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,10 @@ sudo mongod -f /opt/local/etc/mongodb/mongod.conf
 
 echo "generate site"
 ./node_modules/.bin/docpad generate
+find out -type d -exec chmod 775 {} \;
+find out -type f -exec chmod 774 {} \;
 
 echo "publishing!"
-rsync -aP out framallo@framallo.com:public_html
+rsync -aP out/ framallo@framallo.com:public_html
 
 open http://framallo.com
